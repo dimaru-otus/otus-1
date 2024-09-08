50b5ec4c8195fa00512d74ea541761a368ae339e
Dmitriy Borovikov <dmitry@borovikov.name>
2024-09-08 21:28:04 +0300

    Initial Commit

---
+++ otus-hw-1/otus_hw_1App.swift
@@ -0,0 +1,17 @@
+//
+//  otus_hw_1App.swift
+//  otus-hw-1
+//
+//  Created by Dmitriy Borovikov on 08.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct otus_hw_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
